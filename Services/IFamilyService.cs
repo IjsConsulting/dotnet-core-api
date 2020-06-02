734acbca58962582265cba1e51f59ea6769bf53b
ian simpson <iansimpson55@hotmail.com>
2020-06-02 12:36:29 +0100

    seperated Service class

--- Services/IFamilyService.cs
+++ Services/IFamilyService.cs
@@ -7,45 +7,4 @@ namespace dotnet_core_api.Services
     {
         List<Family> GetFamilies();
     }
-
-    public class FamilyService : IFamilyService
-    {
-        public List<Family> GetFamilies()
-        {
-            var families = new List<Family>
-            {
-                new Family
-                {
-                    Id = "Andersen.1",
-                    LastName = "Andersen",
-                    Parents = new Parent[]
-                    {
-                        new Parent { FirstName = "Thomas" },
-                        new Parent { FirstName = "Mary Kay" }
-                    },
-                    Children = new Child[]
-                    {
-                        new Child
-                        {
-                            FirstName = "Henriette Thaulow",
-                            Gender = "female",
-                            Grade = 5,
-                            Pets = new Pet[]
-                            {
-                                new Pet { GivenName = "Fluffy" }
-                            }
-                        }
-                    },
-                    Address = new Address
-                    {
-                        State = "WA",
-                        County = "King",
-                        City = "Seattle"
-                    },
-                    IsRegistered = false
-                }
-            };
-            return families;
-        }
-    }
 }
